c25e3d3b30c32046082e7d0cfb91a2a90ce719d6
Raducu Daniel Teodor <raducudanielteodor@yahoo.com>
2021-12-22 23:49:35 +0200

    victorie! era o eroare in declararea constantei - graba...

--- app/js/index.js
+++ app/js/index.js
@@ -1,7 +1,10 @@
-const btnHamburger = document.querySelector("#btnHamburger").addEventListener("click", function() {
+const btnHamburger = document.querySelector('#btnHamburger');
+
+btnHamburger.addEventListener('click', function() {
     console.log("works")
+    console.log(btnHamburger.classList);
     if (btnHamburger.classList.contains('open')) {
-        btnHamburger.classList.remove('open');
+        btnHamburger.classList.remove('.open');
     }
     else {
         btnHamburger.classList.add('open');
