2240007521112d5c4845bd637b56c989c46aed76
Raducu Daniel Teodor <raducudanielteodor@yahoo.com>
2022-01-02 00:16:44 +0200

    am reparat animatia... lipsea punctul de la clasa in queryS...

--- app/js/index.js
+++ app/js/index.js
@@ -1,7 +1,7 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
-const overlay = document.querySelector('.overlay')
-const fadeElems = document.querySelectorAll('has-fade')
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click', function() {
     // console.log("works")
@@ -23,21 +23,3 @@ btnHamburger.addEventListener('click', function() {
 });
 
 
-// btnHamburger.addEventListener('click', function() {
-//     // console.log("works")
-//     // console.log(btnHamburger.classList);
-//     if (header.classList.contains('open')) { // cand inchizi meniul
-//         header.classList.remove('open');
-//         fadeElems.forEach(function(element) {
-//             element.classList.add('fade-out');
-//             element.classList.remove('fade-in');
-//         })
-//     }
-//     else { // cand deschizi meniul
-//         header.classList.add('open');
-//         fadeElems.forEach(function(element) {
-//             element.classList.add('fade-in');
-//             element.classList.remove('fade-out'); 
-//         })
-//     }
-// });
