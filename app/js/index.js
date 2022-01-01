5ad950cbb574752595b1fc4b0e48432a80989db7
Raducu Daniel Teodor <raducudanielteodor@yahoo.com>
2022-01-02 00:10:37 +0200

    am o problema ca nu imi merge inca animatia de fade de pe header__menu

--- app/js/index.js
+++ app/js/index.js
@@ -1,12 +1,43 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay')
+const fadeElems = document.querySelectorAll('has-fade')
 
 btnHamburger.addEventListener('click', function() {
-    console.log("works")
-    console.log(btnHamburger.classList);
-    if (btnHamburger.classList.contains('open')) {
-        btnHamburger.classList.remove('.open');
+    // console.log("works")
+    // console.log(btnHamburger.classList);
+    if (header.classList.contains('open')) { // cand inchizi meniul
+        header.classList.remove('open');
+        fadeElems.forEach(function(element) {
+            element.classList.add('fade-out');
+            element.classList.remove('fade-in');
+        })
     }
-    else {
-        btnHamburger.classList.add('open');
+    else { // cand deschizi meniul
+        header.classList.add('open');
+        fadeElems.forEach(function(element) {
+            element.classList.add('fade-in');
+            element.classList.remove('fade-out'); 
+        })
     }
 });
+
+
+// btnHamburger.addEventListener('click', function() {
+//     // console.log("works")
+//     // console.log(btnHamburger.classList);
+//     if (header.classList.contains('open')) { // cand inchizi meniul
+//         header.classList.remove('open');
+//         fadeElems.forEach(function(element) {
+//             element.classList.add('fade-out');
+//             element.classList.remove('fade-in');
+//         })
+//     }
+//     else { // cand deschizi meniul
+//         header.classList.add('open');
+//         fadeElems.forEach(function(element) {
+//             element.classList.add('fade-in');
+//             element.classList.remove('fade-out'); 
+//         })
+//     }
+// });
