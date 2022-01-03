e5f787e78a1c749fc0004ba3e1a0e0d155a05cb8
Raducu Daniel Teodor <raducudanielteodor@yahoo.com>
2022-01-03 17:21:25 +0200

    am invatat sa aranjez calea de la fisierul settings.json, sa imi interpretez scss in folderul dist/css

--- app/js/index.js
+++ app/js/index.js
@@ -2,11 +2,13 @@ const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
 const fadeElems = document.querySelectorAll('.has-fade');
+const body = document.querySelector('body');
 
 btnHamburger.addEventListener('click', function() {
     // console.log("works")
     // console.log(btnHamburger.classList);
     if (header.classList.contains('open')) { // cand inchizi meniul
+        body.classList.remove('noscroll');
         header.classList.remove('open');
         fadeElems.forEach(function(element) {
             element.classList.add('fade-out');
@@ -14,6 +16,7 @@ btnHamburger.addEventListener('click', function() {
         })
     }
     else { // cand deschizi meniul
+        body.classList.add('noscroll');
         header.classList.add('open');
         fadeElems.forEach(function(element) {
             element.classList.add('fade-in');
